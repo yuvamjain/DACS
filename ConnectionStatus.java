49e540ecf6c5daf617d8ae28044af4f10e0c3d18
yuvamjain <yuvamjain@gmail.com>
2020-08-06 09:45:18 +0530

    Adding correct package
    Adding correct package

--- ConnectionStatus.java
+++ ConnectionStatus.java
@@ -40,7 +40,7 @@ Navigate your code with ease. Click on function and method calls to jump to thei
  * @author Ian Kaplan, www.bearcave.com, iank@bearcave.com
  */
 package io.kunalk.springaws.dynamoDBweb.service;
-package com.klera.com.gso;
+package com.klera.gso;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
